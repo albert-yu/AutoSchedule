93add41fecf7d6ddea5bb8ed3157ae23993681f9
Albert <albert.yu@gmail.com>
2016-01-08 13:57:14 -0800

    Begin to add GUI

--- src/com/opencsv/GUI.java
+++ src/com/opencsv/GUI.java
@@ -9,20 +9,35 @@ import java.awt.event.*;
  * @author Albert Yu
  *
  */
-public class GUI extends JFrame implements ActionListener
+public class GUI extends JFrame
 {
+   private JTextArea textArea = new JTextArea();
+   private JTextField userInputField = new JTextField();
+   
    public GUI()
    {
       // set the window title
       super("AutoSchedule! Yay!");
       
-   }
-
-   @Override
-   public void actionPerformed(ActionEvent e)
-   {
-      // TODO Auto-generated method stub
+      // get the content pane so we can add stuff to it
+      Container content = getContentPane();
       
+      setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+      setSize(400, 600);
+      setVisible(true);
    }
 
+   private class SubmitListener implements ActionListener
+   {
+      public void actionPerformed(ActionEvent e)
+      {
+         // String input = userInputField.getText() <- contains filename
+      }
+   }
+
+   public static void main(String[] args)
+   {
+      new GUI();
+   }
 }
